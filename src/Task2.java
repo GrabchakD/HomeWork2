b3e24eff15604f4b29a7ca2ea56f5660e288aab6
GrabchakD <dapochemu@gmail.com>
2018-05-07 00:07:56 +0300

    Done

--- src/Task2.java
+++ src/Task2.java
@@ -10,31 +10,27 @@ Given an array of characters char[] length of N.
         Output
         abedcfg - substring "cde" was reversed to "edc".
 */
-/*
+
 
 import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int length = 7;
+        int indexStart = 2;
+        int indexEnd = 4;
+        char[] ch = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
 
+        for (int i = 0; i < ch.length; i++) {
+            if (i > indexStart - 1 && i - 1 <= indexEnd +1) {
+                System.out.print(ch[indexEnd]);
+                indexEnd--;
+            } else {
+                System.out.print(ch[i]);
+            }
 
-    Scanner sc = new Scanner(System.in);
-    int length = sc.nextInt();
-    String s = sc.toString();
-    char toC = s.charAt();
-    char[] chars = new char[length];
-    int start = sc.nextInt();
-    int end = sc.nextInt();
-
-    for (int i = 0; i < chars.length; i++) {
-        chars[i] = toC.();
+        }
     }
+}
 
-    while (end != start) {
-        System.out.println(chars[end]);
-        end--;
-    }
-    }
-
-    SUBSTRING OR CHARAT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-}*/
