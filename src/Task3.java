3b253d7d3ca54c08234443fa5c0866180374b0f9
GrabchakD <dapochemu@gmail.com>
2018-05-06 22:52:11 +0300

    Task 3

--- src/Task3.java
+++ src/Task3.java
@@ -18,5 +18,20 @@ Given a string str and character ch, find the frequency of occurrence ch in str.
 */
 
 
+import java.util.Scanner;
+
 public class Task3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char ch = sc.next ("."). charAt (0);
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (ch == str.charAt(i)) {
+                count ++;
+            }
+        }
+        System.out.println(count);
+    }
 }
